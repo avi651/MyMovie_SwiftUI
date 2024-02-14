38fefb7053482d81df980399e514a39e59be246c
Avinash Kumar <kumaravi651@gmail.com>
2024-02-14 15:44:26 +0530

    Initial Commit

---
+++ MyMovie/MyMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieApp.swift
+//  MyMovie
+//
+//  Created by Avinash on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
